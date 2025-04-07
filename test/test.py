f37b6681bcfe87de303c394183842d0019724477
ruoxining <ruoxining@outlook.com>
2025-04-07 17:38:16 -0400

    [feat] test for different images

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ This module tests the functionality of hidden state extraction model classes.
 import importlib
 import os
 import sys
+from itertools import zip_longest
 from typing import List, Tuple
 
 import pytest
@@ -48,6 +49,7 @@ class TestHiddenStates:
 
     def _model_run(self,
                    config,
+                   group_by: str = 'layer'
                    ) -> List[Tuple[str, Tuple[torch.Tensor], str, str, str]]:
         """Initialize the model and run it."""
         # mock the model run
@@ -78,6 +80,11 @@ class TestHiddenStates:
 
         # TODO: rename the tensors into tuples to be more specific
 
+        if group_by == 'layer':
+            tensors = sorted(tensors, key=lambda x: x[0])
+        elif group_by == 'image_path':
+            tensors = sorted(tensors, key=lambda x: x[3])
+
         return tensors
 
     def _get_last_timestamp(self, tensors: List[Tuple[str, torch.Tensor, str, str, str]]) -> None:
@@ -93,21 +100,23 @@ class TestHiddenStates:
         """Test if hidden states are floats."""
         self._import()
 
-        # TODO: set image path
-
         # init config
-        sys.argv = ['test/test.py', '-c', config_path, '--debug']
+        image_path = 'test/data/single'
+        sys.argv = ['test/test.py',
+                    '-c', config_path,
+                    '-i', image_path,
+                    '--debug'
+                    ]
         config = self.Config()
 
         # get the model output
         # TODO: to save the model output globaly to save time
         model_outputs = self._model_run(config=config)
 
-        # check that input_ids are integers
+        # check if the tensor is a float
         for model_output in model_outputs:
             _, tensors, _, _, _ = model_output
             for tensor in tensors:
-                # Check if the tensor is a float
                 assert tensor.dtype in [torch.float, torch.float32, torch.float64, torch.bfloat16], \
                     f'Tensor is not a float: {tensor.dtype}'
 
@@ -117,20 +126,57 @@ class TestHiddenStates:
 
         image_path = 'test/data/single'
 
-        # get model outputs
-        sys.argv = ['test/test.py', '-c', config_path, '-i', image_path, '--debug']
-        config1 = self.Config()
-        model_outputs1 = self._model_run(config=config1)
+        # get model outputs 1
+        sys.argv = ['test/test.py',
+                    '-c', config_path,
+                    '-i', image_path,
+                    '--debug'
+                    ]
+        config_1 = self.Config()
+        model_outputs_1 = self._model_run(config=config_1)
 
-        # init config2
-        sys.argv = ['test/test.py', '-c', config_path, '-i', image_path, '--debug']
-        config2 = self.Config()
-        model_outputs2 = self._model_run(config=config2)
+        # get model outputs 2
+        sys.argv = ['test/test.py',
+                    '-c', config_path,
+                    '-i', image_path,
+                    '--debug'
+                    ]
+        config_2 = self.Config()
+        model_outputs_2 = self._model_run(config=config_2)
 
         # check that the outputs are the same
-        for model_output1, model_output2 in zip(model_outputs1, model_outputs2):
+        for model_output1, model_output2 in zip(model_outputs_1, model_outputs_2):
             _, tensor1, _, _, _ = model_output1
             _, tensor2, _, _, _ = model_output2
 
             assert torch.allclose(tensor1, tensor2), \
                 f'Tensors are not close: {tensor1} and {tensor2}'
+
+    def test_hidden_states_diff_inputs(self, config_path: str) -> None:
+        """Check if the different inputs generate different outputs."""
+        self._import()
+
+        image_path = 'test/data/diff'
+
+        # get model outputs
+        sys.argv = ['test/test.py',
+                    '-c', config_path,
+                    '-i', image_path,
+                    '--debug'
+                    ]
+        config = self.Config()
+        model_outputs = self._model_run(config=config)
+
+        # check image layers
+        n_layers = len(self._get_unique_layers(config))
+
+        def grouper(n, iterable, fillvalue=None):
+            args = [iter(iterable)] * n
+            return zip_longest(*args, fillvalue=fillvalue)
+
+        for model_output in grouper(n_layers, model_outputs):
+            _, tensor1, _, _, _ = model_output[0]
+            _, tensor2, _, _, _ = model_output[1]
+
+            assert not torch.allclose(tensor1, tensor2), \
+                f'Tensors are close: {tensor1} and {tensor2}'
