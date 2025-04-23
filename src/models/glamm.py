f1993144264ef628a061690aa73e5c1849902263
e48huang <e48huang@uwaterloo.ca>
2025-04-23 15:46:22 -0400

    [feat] Initial commit for organizing what needs to be overriden.

--- src/models/glamm.py
+++ src/models/glamm.py
@@ -3,7 +3,12 @@
 File for providing model implementations for any models using AutoModel.
 """
 
-from transformers import AutoModelForVision2Seq
+import logging
+
+import cv2
+import torch
+from transformers import (AutoModelForVision2Seq, AutoTokenizer,
+                          CLIPImageProcessor)
 
 from .base import ModelBase
 from .config import Config
@@ -21,6 +26,8 @@ class GlammModel(ModelBase):
         # initialize the parent class
         super().__init__(config)
 
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
+
     def _load_specific_model(self):
         """Overridden function to populate self.model."""
         self.model = AutoModelForVision2Seq.from_pretrained(
@@ -32,8 +39,17 @@ class GlammModel(ModelBase):
         )
 
     def _init_processor(self) -> None:
-        """Don't set the self.processor as it's not needed for Glamm."""
-        return
+        """Set the self.processor to follow the example given.
+
+        This should follow the processor setting and tokenizers under:
+        https://github.com/mbzuai-oryx/groundingLMM/blob/main/app.py
+        """
+        logging.debug(
+            f'GLAMM has vision tower: {self.model.config.vision_tower}'
+        )
+        self.processor = CLIPImageProcessor.from_pretrained(
+            self.model.config.vision_tower
+        )
 
     def _generate_prompt(self) -> str:
         """Generates the GLaMM model prompt which will not use the chat template.
@@ -42,3 +58,53 @@ class GlammModel(ModelBase):
             str: The prompt to return, set by the config.
         """
         return self.config.prompt
+
+    def _load_input_data(self, prompt, img_path) -> dict:
+        """Generate the processor argument to be input into the processor.
+
+        Args:
+            prompt (str): The generated prompt string with the input text and
+                the image labels.
+            img_path (str): The specified image path.
+
+        Returns:
+            dict: The corresponding processor arguments per image and prompt.
+        """
+        if img_path is None:
+            raise ValueError('GLAMM cannot have text-only generation.')
+
+        image_np = cv2.imread(img_path)
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        global_enc_image = self.processor.preprocess(
+            image_np, return_tensors='pt'
+        )['pixel_values'][0].unsqueeze(0).bfloat16()
+
+        input_ids = self.tokenizer(prompt, return_tensors='pt')
+        input_ids = input_ids.unsqueeze(0)
+
+        return {
+            'global_enc_image': global_enc_image,
+            'input_ids': input_ids,
+            'bboxes': None  # set this to none for now
+        }
+
+    def _forward(self, data):
+        """Given some input data, performs a single forward pass.
+
+        This function itself can be overriden, while _hook_and_eval
+        should be left in tact.
+
+        Args:
+            data (BatchFeature): The given data tensor.
+        """
+        with torch.no_grad():
+            _ = self.model.generate(
+                images=data['global_enc_image'],
+                input_ids=data['input_ids'],
+                bboxes=data['bboxes'],
+                max_new_tokens=512,
+                num_beams=1,
+                output_hidden_states=True,
+                return_dict_in_generate=True
+            )
+        logging.debug('Completed forward pass')
