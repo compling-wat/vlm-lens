d35673ffe73761830313dee8651d0cd5cbfae3f9
Freda Shi <fhs@uwaterloo.ca>
2025-08-05 01:49:02 -0400

    [fix] fixes clip code formats.

--- src/models/clip/clip.py
+++ src/models/clip/clip.py
@@ -12,7 +12,7 @@ from src.models.config import Config
 class ClipModel(ModelBase):
     """Clip model implementation."""
 
-    def __init__(self, config: Config):
+    def __init__(self, config: Config) -> None:
         """Initialization of the clip model.
 
         Args:
@@ -21,7 +21,7 @@ class ClipModel(ModelBase):
         # initialize the parent class
         super().__init__(config)
 
-    def _load_specific_model(self):
+    def _load_specific_model(self) -> None:
         """Overridden function to populate self.model."""
         self.model = CLIPModel.from_pretrained(
             self.model_path, **self.config.model
@@ -31,8 +31,11 @@ class ClipModel(ModelBase):
             )
         )
 
-    def _generate_prompt(self, prompt) -> str:
+    def _generate_prompt(self, prompt: str) -> str:
         """Generates the CLIP model prompt which will not use the chat template.
+
+        Args:
+            prompt (str): The prompt to return, set by the config.
 
         Returns:
             str: The prompt to return, set by the config.
