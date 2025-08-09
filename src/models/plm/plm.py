18b658f43ffa4464fc17430d54da046e9b2d8cbc
wonderwind271 <shuyuwu@umich.edu>
2025-08-09 15:16:47 -0400

    finished version of plm

--- src/models/plm/plm.py
+++ src/models/plm/plm.py
@@ -34,21 +34,22 @@ class PlmModel(ModelBase):
                 self.model_path
             )
         )
+        self.model.to(self.config.device)
 
     def _init_processor(self) -> None:
         """Initialize the self.processor by loading from the path."""
         self.processor = AutoProcessor.from_pretrained(self.model_path, use_fast=True)
 
-    def _generate_prompt(self, prompt: str) -> str:
-        """Generates the PLM model prompt which will not use the chat template.
+    # def _generate_prompt(self, prompt: str) -> str:
+    #     """Generates the PLM model prompt which will not use the chat template.
 
-        Args:
-            prompt (str): The input prompt to be processed.
+    #     Args:
+    #         prompt (str): The input prompt to be processed.
 
-        Returns:
-            str: The prompt to return, set by the config.
-        """
-        return f'USER: <image>\n{prompt} ASSISTANT:'  # Taken directly from official doc
+    #     Returns:
+    #         str: The prompt to return, set by the config.
+    #     """
+    #     return f'USER: <image>\n{prompt} ASSISTANT:'  # Taken directly from official doc
 
     def _forward(self, data: BatchFeature) -> None:
         """Given some input data, performs a single forward pass.
@@ -59,6 +60,7 @@ class PlmModel(ModelBase):
         Args:
             data (BatchFeature): The given data tensor.
         """
+        data.to(self.config.device)
         with torch.no_grad():
             _ = self.model.generate(**data, **self.config.forward)
         logging.debug('Completed forward pass...')
